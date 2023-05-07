d3423ccd736a4a88b12f4681a9954237b5b0419a
香蕉木头人520 <96758259+huanghongtao520@users.noreply.github.com>
2023-05-07 21:25:37 +0800

    14

--- springboot-redis1/src/main/java/com/example/springbootredis1/huang/Controller/UserController.java
+++ springboot-redis1/src/main/java/com/example/springbootredis1/huang/Controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.springbootredis1.huang.Controller;
 
 import com.example.springbootredis1.huang.pojo.User;
+import com.example.springbootredis1.huang.utils.RedisUtil;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,11 +17,19 @@ public class UserController {
     @Qualifier("redisTemplate")
     //使用自定义的redisTemplate
     private RedisTemplate redisTemplate;
+
+    // 导入自定义的工具类
+    // redisTemplate.opsForValue().set("user",user);
+    // 直接通过redisUtil.set()设置，简化开发
+    @Autowired
+    private RedisUtil redisUtil;
+
     @RequestMapping("/*")
     @ResponseBody
     public  String test1() throws JsonProcessingException {
         User user = new User("黄宏涛", 22);
-        redisTemplate.opsForValue().set("user",user);
-        return redisTemplate.opsForValue().get("user").toString();
+        //redisTemplate.opsForValue().set("user",user);
+        redisUtil.set("user",user);
+        return redisUtil.get("user").toString();
     }
 }
